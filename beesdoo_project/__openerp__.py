333694995ed9753158aec7818be3229fd720c821
Thibault Francois <tfr@openerp.com>
2015-12-05 14:25:52 +0100

    [INIT][SPEC1001] First module task modification

---
+++ beesdoo_project/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Beescoop Project Management", 
+
+    'summary': """
+		Module that customize project management for Beescoop 
+     """,
+
+    'description': """
+    """,
+
+    'author': "Beescoop - Cellule IT",
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'category': 'Project Management',
+    'version': '0.1',
+
+    'depends': ['project'],
+
+    'data': [
+        'views/task.xml',
+    ],
+}
