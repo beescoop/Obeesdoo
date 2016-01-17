2ad1e2386a7a34ff7d8be1dbc4611099f35ba20b
Thibault Francois <tfr@openerp.com>
2016-01-17 23:16:01 +0100

    [ADD] S0006 : first_name and last_name on res_partner

---
+++ beesdoo_base/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Beescoop Base Module",
+
+    'summary': """
+		Module that customize the base module and contains some python tools
+     """,
+
+    'description': """
+    """,
+
+    'author': "Beescoop - Cellule IT",
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'category': 'Project Management',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/partner.xml',
+    ],
+}
