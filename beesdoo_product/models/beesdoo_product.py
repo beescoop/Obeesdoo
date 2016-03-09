fa68710b3b21a535a50c9b2c95503107cb5360f4
EliseDup <Elise@wifi-secure1-933.sri.ucl.ac.be>
2016-03-09 15:44:50 +0100

    S001

--- beesdoo_product/models/beesdoo_product.py
+++ beesdoo_product/models/beesdoo_product.py
@@ -8,7 +8,18 @@ class BeesdooProduct(models.Model):
     local_label = fields.Many2one('beesdoo.product.label', domain = [('type', '=', 'local')])
     fair_label = fields.Many2one('beesdoo.product.label', domain = [('type', '=', 'fair')])
     origin_label = fields.Many2one('beesdoo.product.label', domain = [('type', '=', 'delivery')])
-
+    
+    main_seller_id = fields.Many2one('res.partner', compute='_compute_main_seller_id', store=True)
+    
+    @api.one
+    @api.depends('seller_ids')
+    def _compute_main_seller_id(self):
+        # Ce champs doit être champs calculé qui va chercher 
+        # le vendeur associé qui a la date de début la plus récente et plus petite qu’aujourd’hui
+        self.main_seller_id = sorted(self.seller_ids, key=lambda seller: seller.date_start, reverse=True)[0].name
+        
+    
+        
 class BeesdooProductLabel(models.Model):
     _name = "beesdoo.product.label"
 
