c2add4b5845505aa5623ae2210e4ca192437cba9
ufaks <excellm@yandex.ru>
2016-03-10 16:56:59 +0500

    fix

--- pos_keyboard/__openerp__.py
+++ pos_keyboard/__openerp__.py
@@ -3,10 +3,9 @@
     'name': "Keyboard support in Point Of Sale",
     'author': "IT-Projects LLC, Ivan Yelizariev",
     'summary': 'Module allows to use usual keyboard (not virtual one) in Point of Sale',
-    "website": "https://www.it-projects.info",
+    "website": "https://it-projects.info",
     'images': ['images/keyboard.png'],
     'category': 'Point Of Sale',
-    'license': 'LGPL-3',
     'version': '1.0.1',
     'depends': ['point_of_sale'],
     'data': [
