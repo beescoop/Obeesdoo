f439d8dcc78ae67ddd82661ee2698391aaa3348e
gregoire <gregoire@page-karma.com>
2016-04-16 19:09:20 +0200

    add total by unit and clean xml

--- beesdoo_product/models/beesdoo_product.py
+++ beesdoo_product/models/beesdoo_product.py
@@ -18,21 +18,31 @@ class BeesdooProduct(models.Model):
     total = fields.Float(compute='get_total')
     total_with_vat = fields.Float(compute='get_total_with_vat')
 
+    total_with_vat_by_unit = fields.Float(compute='get_total_with_vat_by_unit')
+
     @api.one
     @api.depends('weight', 'display_unit')
     def get_display_weight(self):
         if self.display_unit:
             self.display_weight = self.weight / self.display_unit.factor
 
+    @api.one
     def get_total(self):
         price_ht = self.env['product.pricelist'].search([])[0].price_get(self.id, 1)[1]
         self.total = price_ht
 
+    @api.one
     def get_total_with_vat(self):
         tax_amount_sum = 0.0
-        for tax in self.taxes_id:
-            tax_amount_sum = tax_amount_sum + tax.amount
+        if hasattr(self, 'taxes_id'):
+            for tax in self.taxes_id:
+                tax_amount_sum = tax_amount_sum + tax.amount
         self.total_with_vat = self.total * (100.0 + tax_amount_sum) / 100
+
+    @api.one
+    def get_total_with_vat_by_unit(self):
+        if self.display_weight > 0:
+            self.total_with_vat_by_unit = self.total_with_vat/self.display_weight
 
 
 class BeesdooProductLabel(models.Model):
