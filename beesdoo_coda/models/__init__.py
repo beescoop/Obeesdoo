f0b6ff4fd9d93685629ef7fdc2e6c7fc7229668c
EliseDup <Elise@mac-1o0-339.local>
2016-06-06 16:56:50 +0200

    Merge branch '9.0' of https://github.com/beescoop/Obeesdoo into 9.0-S0025-Elise
    # Conflicts:
    #	beesdoo_base/__openerp__.py

---
+++ beesdoo_coda/models/__init__.py
@@ -0,0 +1,1 @@
+import bank_statement
---
+++ beesdoo_product/wizard/label_printing_utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+
+class RequestLabelPrintingWizard(models.TransientModel):
+
+    _name = 'label.printing.wizard'
+
+    def _get_selected_products(self):
+        return self.env.context['active_ids']
+
+    product_ids = fields.Many2many('product.template', default=_get_selected_products)
+
+
+    @api.one
+    def request_printing(self):
+        self.product_ids.write({'label_to_be_printed' : True})
+
+
+    @api.one
+    def set_as_printed(self):
+        self.product_ids.write({'label_to_be_printed' : False, 'label_last_printed' : fields.Datetime.now()})
