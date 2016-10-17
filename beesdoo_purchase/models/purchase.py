72372cd12c055a378359f69f271c60dfe1d6dac4
EliseDup <Elise@mac-1o0-339.local>
2016-10-17 17:02:47 +0200

    S0025 : fix. Add subtypes discussion and those linked to purchase_order

--- beesdoo_purchase/models/purchase.py
+++ beesdoo_purchase/models/purchase.py
@@ -8,10 +8,11 @@ class PurchaseOrder(models.Model):
     def create(self, vals):
         purchase_order = super(PurchaseOrder, self).create(vals)
         command_contact = self.env.ref('beesdoo_base.commande_beescoop', raise_if_not_found=False)
-        types = self.env['mail.message.subtype'].search([('res_model','=','purchase.order')])
+        types = self.env['mail.message.subtype'].search(['|',('res_model','=','purchase.order'),('name','=','Discussions')])
         
         if command_contact:
-            self.message_subscribe(partner_ids=command_contact, subtype_ids=types)
-            # self.env['mail.followers'].create({'res_model' : 'purchase.order', 'res_id' : purchase_order.id, 'partner_id' : command_contact.id}) 
-            
+            self.env['mail.followers'].create({'res_model' : 'purchase.order', 
+                                               'res_id' : purchase_order.id, 
+                                               'partner_id' : command_contact.id,
+                                               'subtype_ids': [(6, 0, types.ids)]})
         return purchase_order
