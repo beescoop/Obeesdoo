0be6b8d655a65eea9173b2635cc8ac63942fc90c
EliseDup <Elise@mac-1o0-339.local>
2016-10-18 18:51:16 +0200

    Account module added, make invoice_date as required.

---
+++ beesdoo_account/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Beescoop  Account Module",
+
+    'summary': """
+		Module that customize account module for Beescoop
+     """,
+
+    'description': """
+    """,
+
+    'author': "Beescoop - Cellule IT",
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'category': 'Account Module',
+    'version': '0.1',
+
+    'depends': ['account'],
+
+    'data': [],
+}
