8ae7946d7a1b6d5a226b303d1b52e3105033e378
Thibault Francois <tfr@openerp.com>
2016-10-26 10:26:14 +0200

    [CLEAN] remove trailing space

--- beesdoo_purchase/models/purchase.py
+++ beesdoo_purchase/models/purchase.py
@@ -9,10 +9,10 @@ class PurchaseOrder(models.Model):
         purchase_order = super(PurchaseOrder, self).create(vals)
         command_contact = self.env.ref('beesdoo_base.commande_beescoop', raise_if_not_found=False)
         types = self.env['mail.message.subtype'].search(['|',('res_model','=','purchase.order'),('name','=','Discussions')])
-        
+
         if command_contact:
-            self.env['mail.followers'].create({'res_model' : 'purchase.order', 
-                                               'res_id' : purchase_order.id, 
+            self.env['mail.followers'].create({'res_model' : 'purchase.order',
+                                               'res_id' : purchase_order.id,
                                                'partner_id' : command_contact.id,
                                                'subtype_ids': [(6, 0, types.ids)]})
         return purchase_order
