3424f103e2d1c2d7f1c5187877b9a2ff03f17b20
Thibault Francois <francois.th@gmail.com>
2017-01-10 00:25:43 +0100

    [ADD] info on super cooperaive status on res.partner, it's not meant to be change here

--- beesdoo_shift/models/res_users.py
+++ beesdoo_shift/models/res_users.py
@@ -5,3 +5,8 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     super = fields.Boolean("Super Cooperative")
+    
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    super = fields.Boolean(related='user_ids.super', string="Super Cooperative", readonly=True)
