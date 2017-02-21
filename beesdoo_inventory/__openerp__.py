b688266c670c6f0a1735ecd5e7fd187b5d75099d
tfrancoi <francois.th@gmail.com>
2017-02-21 22:16:15 +0100

    [S0046] 0046 - Faciliter la réception de marchandises: Add a button to copy all qty in done column at reception

--- beesdoo_inventory/__openerp__.py
+++ beesdoo_inventory/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['delivery'],
+    'depends': ['delivery', 'beesdoo_base'],
 
     # always loaded
     'data': [
