1eac5c3a5adf36d74c9d2b42a736ab99de13da9a
Thibault Francois <francois.th@gmail.com>
2017-02-26 15:33:37 +0100

    [ADD] Mass editing module

---
+++ mass_editing/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Mass Editing',
+    'version': '9.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., '
+              'Odoo Community Association (OCA)',
+    'contributors': [
+        'Oihane Crucelaegui <oihanecrucelaegi@gmail.com>',
+        'Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>',
+    ],
+    'category': 'Tools',
+    'website': 'http://www.serpentcs.com',
+    'license': 'GPL-3 or any later version',
+    'summary': 'Mass Editing',
+    'uninstall_hook': 'uninstall_hook',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/mass_editing_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
