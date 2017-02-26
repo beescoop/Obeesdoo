9fe5f9e466dfdedd85112ffa79f4a9e55796b56e
Thibault Francois <francois.th@gmail.com>
2017-02-26 16:32:42 +0100

    [CLEAN] Remove pyc file and fix the compute field

--- pos_access_right/models/pos_config.py
+++ pos_access_right/models/pos_config.py
@@ -11,65 +11,44 @@ class PosConfig(models.Model):
 
     group_negative_qty_id = fields.Many2one(
         comodel_name='res.groups',
-        compute='_compute_group_negative_qty_id',
+        compute='_compute_group',
         string='Point of Sale - Allow Negative Quantity',
         help="This field is there to pass the id of the 'PoS - Allow Negative"
         " Quantity' Group to the Point of Sale Frontend.")
 
     group_discount_id = fields.Many2one(
         comodel_name='res.groups',
-        compute='_compute_group_discount_id',
+        compute='_compute_group',
         string='Point of Sale - Allow Discount',
         help="This field is there to pass the id of the 'PoS - Allow Discount'"
         " Group to the Point of Sale Frontend.")
 
     group_change_unit_price_id = fields.Many2one(
         comodel_name='res.groups',
-        compute='_compute_group_change_unit_price_id',
+        compute='_compute_group',
         string='Point of Sale - Allow Unit Price Change',
         help="This field is there to pass the id of the 'PoS - Allow Unit"
         " Price Change' Group to the Point of Sale Frontend.")
 
     group_multi_order_id = fields.Many2one(
         comodel_name='res.groups',
-        compute='_compute_group_multi_order_id',
+        compute='_compute_group',
         string='Point of Sale - Many Orders',
         help="This field is there to pass the id of the 'PoS - Many Orders"
         " Group to the Point of Sale Frontend.")
 
     group_delete_order_id = fields.Many2one(
         comodel_name='res.groups',
-        compute='_compute_group_delete_order_id',
+        compute='_compute_group',
         string='Point of Sale - Delete Order',
         help="This field is there to pass the id of the 'PoS - Delete Order'"
         " Group to the Point of Sale Frontend.")
 
     @api.multi
-    def _compute_group_negative_qty_id(self):
+    def _compute_group(self):
         for config in self:
-            self.group_negative_qty_id = \
-                self.env.ref('pos_access_right.group_negative_qty')
-
-    @api.multi
-    def _compute_group_discount_id(self):
-        for config in self:
-            self.group_discount_id = \
-                self.env.ref('pos_access_right.group_discount')
-
-    @api.multi
-    def _compute_group_change_unit_price_id(self):
-        for config in self:
-            self.group_change_unit_price_id = \
-                self.env.ref('pos_access_right.group_change_unit_price')
-
-    @api.multi
-    def _compute_group_multi_order_id(self):
-        for config in self:
-            self.group_multi_order_id = \
-                self.env.ref('pos_access_right.group_multi_order')
-
-    @api.multi
-    def _compute_group_delete_order_id(self):
-        for config in self:
-            self.group_delete_order_id = \
-                self.env.ref('pos_access_right.group_delete_order')
+            config.group_negative_qty_id = self.env.ref('pos_access_right.group_negative_qty')
+            config.group_discount_id = self.env.ref('pos_access_right.group_discount')
+            config.group_change_unit_price_id = self.env.ref('pos_access_right.group_change_unit_price')
+            config.group_multi_order_id = self.env.ref('pos_access_right.group_multi_order')
+            config.group_delete_order_id = self.env.ref('pos_access_right.group_delete_order')
