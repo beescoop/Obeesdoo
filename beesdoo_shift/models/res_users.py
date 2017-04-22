8595a9366aa67408c000b84896683e0f3d6ec4e8
Rémy Taymans <remytaymans@gmail.com>
2017-04-22 15:24:08 +0200

    [ADD][TASK s0048a] working mode
    [CHANGE][TASK s0048] occasional -> irregular

--- beesdoo_shift/models/res_users.py
+++ beesdoo_shift/models/res_users.py
@@ -5,8 +5,21 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     super = fields.Boolean("Super Cooperative")
+    working_mode = fields.Selection(
+        [
+            ('regular', 'Regular worker'),
+            ('irregular', 'Irregular worker'),
+            ('exempt', 'Exempted'),
+        ],
+        string="Working mode",
+    )
     
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     super = fields.Boolean(related='user_ids.super', string="Super Cooperative", readonly=True)
+    working_mode = fields.Selection(
+        related='user_ids.working_mode',
+        string="Working mode",
+        readonly=True,
+    )
