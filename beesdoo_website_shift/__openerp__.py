7e1e4409b9006192ed342e1a979d61779af7b455
Rémy Taymans <remytaymans@gmail.com>
2017-10-13 11:57:24 +0200

    [FIX] beesdoo_portal_shift: change name
    Change name from beesdoo_portal_shift to beesdoo_website_shift.

---
+++ beesdoo_website_shift/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Beescoop Shift Website',
+
+    'summary': """
+        Show available shifts for regular and irregular workers in
+        portal.
+    """,
+    'description': """
+    """,
+
+    'author': 'Rémy Taymans',
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'category': 'Cooperative management',
+    'version': '0.1',
+
+    'depends': ['website', 'beesdoo_shift'],
+
+    'data': [
+        'views/shift_website_templates.xml',
+        'data/config_data.xml',
+    ]
+}
