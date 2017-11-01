48da2be7fb661e500fab121fb0ef232ebb4fde1b
Thibault Francois <francois.th@gmail.com>
2017-11-01 17:34:19 +0100

    FIX stage status sequence FIX default value for today on status FIX default value for stage_id that allow to manually create shift ADD un subscribed from shift during holidays ADD Cannot subscribe to new holidays when you've already planned some holidays and they are not over yet

--- beesdoo_shift/wizard/holiday.py
+++ beesdoo_shift/wizard/holiday.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
 
 class Subscribe(models.TransientModel):
     _name = 'beesdoo.shift.holiday'
@@ -12,4 +13,7 @@ class Subscribe(models.TransientModel):
     def holidays(self):
         self = self._check() #maybe a different group
         status_id = self.env['cooperative.status'].search([('cooperator_id', '=', self.cooperator_id.id)])
+        if status_id.holiday_end_time >= status_id.today:
+            raise ValidationError(_("You cannot encode new holidays since the previous holidays encoded are not over yet"))
         status_id.sudo().write({'holiday_start_time': self.holiday_start_day, 'holiday_end_time': self.holiday_end_day})
+        self.env['beesdoo.shift.shift'].sudo().unsubscribe_from_today([self.cooperator_id.id], today=self.holiday_start_day, end_date=self.holiday_end_day)
