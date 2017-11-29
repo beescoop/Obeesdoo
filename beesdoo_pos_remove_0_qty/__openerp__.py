170e5f483b7230880332d4b9df2c7d6f7b36595f
Houssine BAKKALI <houssine.bakkali@gmail.com>
2017-11-29 21:35:50 +0100

    Update __openerp__.py

--- beesdoo_pos_remove_0_qty/__openerp__.py
+++ beesdoo_pos_remove_0_qty/__openerp__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# Copyright (C) 2016-Today: BEES coop (<http://www.bees-coop.be/>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
@@ -9,7 +8,6 @@
     'version': '9.0.1.0.0',
     'category': 'Custom',
     'summary': 'Remove pos order line with quantity set to 0',
-    'author': 'La Louve - Sylvain LE GAL',
     'author': 'BEES coop - Houssine BAKKALI',
     'website': 'http://www.bees-coop.be',
     'depends': [
