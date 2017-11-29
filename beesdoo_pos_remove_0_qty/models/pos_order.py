0be3495bb60c8049b49f6121718c1a4bd4296e5c
Houssine BAKKALI <houssine.bakkali@gmail.com>
2017-11-29 21:34:19 +0100

    Add files via upload

---
+++ beesdoo_pos_remove_0_qty/models/pos_order.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    @api.model
+    def _process_order(self, order):
+        print order
+        print order['lines']
+        lines = order['lines']
+        order['lines'] = [l for l in lines if l[2]['qty'] !=0]
+        print order['lines']
+        print order
+                
+        return super(PosOrder, self)._process_order(order)
