5b525b9881ca6e06cbc00c8751b7f8d60b35f8d4
Houssine BAKKALI <houssine.bakkali@gmail.com>
2017-12-04 16:22:18 +0100

    add description

--- beesdoo_pos_remove_0_qty/__openerp__.py
+++ beesdoo_pos_remove_0_qty/__openerp__.py
@@ -9,6 +9,10 @@
     'category': 'Custom',
     'summary': 'Remove pos order line with quantity set to 0',
     'description': """
+    This module fix the issue on picking when there is two lines on 
+    the pos order for the same product, with one of lines with a 0 quantity.
+    The lines at 0 are removed before the pos order is processed to avoid 
+    such issue.
     """,
     'author': 'BEES coop - Houssine BAKKALI',
     'website': 'http://www.bees-coop.be',
