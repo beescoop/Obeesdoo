15aedabbe3181a4adde131996fa50d930da2928b
Rémy Taymans <remytaymans@gmail.com>
2017-12-15 11:06:49 +0100

    [ADD] Add website theme for BEES coop

---
+++ beesdoo_website_theme/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'BEES coop Website Theme',
+
+    'summary': """
+        Apply BEES coop design rules.
+    """,
+    'description': """
+    """,
+
+    'author': 'Rémy Taymans',
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'license': "AGPL-3",
+    'category': 'Themes',
+    'version': '9.0.0.1',
+
+    'depends': ['website'],
+
+    'data': [
+        'views/assets.xml',
+    ]
+}
