cebdfee073c528610366f77fdff0beae670defc9
Thibault Francois <francois.th@gmail.com>
2018-10-07 22:33:51 +0200

    [FIX] bad import to remove tools file

--- beesdoo_base/models/partner.py
+++ beesdoo_base/models/partner.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _
 from openerp.exceptions import ValidationError
-from openerp.addons.beesdoo_base.tools import concat_names
 
 class Partner(models.Model):
 
