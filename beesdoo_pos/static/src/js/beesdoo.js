4d9c4a50779dd4b408c68be61849eff4ef6aada3
robinkeunen <robin@keunen.net>
2018-10-19 11:21:40 +0200

    [REFACTO] rename js imports

--- beesdoo_pos/static/src/js/beesdoo.js
+++ beesdoo_pos/static/src/js/beesdoo.js
@@ -1,7 +1,7 @@
 odoo.define('beescoop.pos', function (require) {
     "use strict";
-    var module   = require("point_of_sale.screens");
-    var Model = require('web.DataModel');
+    var screens = require("point_of_sale.screens");
+    var DataModel = require('web.DataModel');
     var set_customer_info = function(el_class, value, prefix) {
         var el = this.$(el_class);
         el.empty();
@@ -13,14 +13,15 @@ odoo.define('beescoop.pos', function (require) {
         }
     }
 
-    module.ReceiptScreenWidget = module.ReceiptScreenWidget.include({
+
+    screens.ReceiptScreenWidget = screens.ReceiptScreenWidget.include({
         send : function() {
             var self = this;
             var loaded = new $.Deferred();
             var order = this.pos.get_order().name;
-            var records = new Model('pos.order').call('send_order', [order], {}, { shadow: false, timeout: 10000});
+            var records = new DataModel('pos.order').call('send_order', [order], {}, { shadow: false, timeout: 10000});
             records.then(function(result){
-                var el = self.$('.message-send')
+                var el = self.$('.message-send');
                 el.empty();
                 el.append('<h2>' + result + '</h2>');
             },function(err){
@@ -41,9 +42,9 @@ odoo.define('beescoop.pos', function (require) {
             var self = this;
             this.$('.message-send').empty();
         },
-    })
+    });
 
-    module.ActionpadWidget = module.ActionpadWidget.include({
+    screens.ActionpadWidget = screens.ActionpadWidget.include({
         renderElement : function() {
             var self = this;
             var loaded = new $.Deferred();
@@ -53,7 +54,7 @@ odoo.define('beescoop.pos', function (require) {
 
             }
             var customer_id = this.pos.get_client().id;
-            var res = new Model('res.partner').call('get_eater',
+            var res = new DataModel('res.partner').call('get_eater',
                     [ customer_id ], undefined, { shadow: true, timeout: 1000});
             res.then(function(result) {
                 set_customer_info.call(self, '.customer-delegate1', result[0], 'Eater 1: ');
@@ -65,7 +66,7 @@ odoo.define('beescoop.pos', function (require) {
         },
     });
 
-    module.PaymentScreenWidget.include({
+    screens.PaymentScreenWidget.include({
         render_customer_info : function() {
             var self = this;
             var loaded = new $.Deferred();
@@ -73,7 +74,7 @@ odoo.define('beescoop.pos', function (require) {
                 return
             }
             var customer_id = this.pos.get_client().id;
-            var res = new Model('res.partner').call('get_eater', [ customer_id ], undefined, { shadow: true, timeout: 1000});
+            var res = new DataModel('res.partner').call('get_eater', [ customer_id ], undefined, { shadow: true, timeout: 1000});
             res.then(function(result) {
                 set_customer_info.call(self, '.customer-name', self.pos.get_client().name);
                 set_customer_info.call(self, '.customer-delegate1', result[0], 'Eater 1: ');
