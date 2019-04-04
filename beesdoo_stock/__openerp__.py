bbd34ba3a38b59381fba8f4000d15805671dd331
Rémy Taymans <remytaymans@gmail.com>
2019-04-04 13:50:59 +0200

    [IMP] b_stock: Manifest
    Change the name of the module in the manifest and change description.

--- beesdoo_stock/__openerp__.py
+++ beesdoo_stock/__openerp__.py
@@ -3,7 +3,7 @@
 # Nicolas Jamoulle, <nicolas@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Action on products in receipt",
+    "name": "BEES coop Stock",
     "version": "9.0.1.0",
     "depends": [
         'stock',
@@ -12,7 +12,7 @@
     "license": "AGPL-3",
     "website": "www.coopiteasy.be",
     "description": """
-        Enable action on multiple lines of a receipt via checkboxes
+        Enable action on multiple products of a stock receipt
     """,
     "data": [
         'views/stock_view.xml',
