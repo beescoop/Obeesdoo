2b3918314bd74727fa34a8642f6866b2876cbd02
Rémy Taymans <remytaymans@gmail.com>
2019-06-02 22:49:00 +0200

    [ADD] b_shift: Set configurable limit to unsubscribe regular worker
    This adds a configurable limit under which the regular worker is
    unsubscribed. The regular worker is unsubscribed when the sum of his
    shift counter and his compensation shift counter reach the configured
    limit.

--- beesdoo_shift/__openerp__.py
+++ beesdoo_shift/__openerp__.py
@@ -13,7 +13,7 @@
     'website': "https://github.com/beescoop/Obeesdoo",
 
     'category': 'Cooperative management',
-    'version': '9.0.1.1.1',
+    'version': '9.0.1.2.0',
 
     'depends': ['beesdoo_base'],
 
