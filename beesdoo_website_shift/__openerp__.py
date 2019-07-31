24d193d9f353ce0c6b281783d91d4f0c5fe6fe5f
Rémy Taymans <remytaymans@gmail.com>
2019-07-31 18:07:07 +0200

    [CHG] b_w_shift: Configure highlight of task template
    Task templates are now highlighted based on a highlight rule.

--- beesdoo_website_shift/__openerp__.py
+++ beesdoo_website_shift/__openerp__.py
@@ -16,7 +16,7 @@
 
     'author': 'Rémy Taymans',
     'license': 'AGPL-3',
-    'version': '9.0.2.2.0',
+    'version': '9.0.2.3.0',
     'website': "https://github.com/beescoop/Obeesdoo",
 
     'category': 'Cooperative management',
