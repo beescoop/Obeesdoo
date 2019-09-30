b0ca0e098ec71890075d258ea7d757010a8126da
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2019-09-30 11:49:51 +0200

    pos_price_to_weight module : migration to version 11.0

--- pos_price_to_weight/__manifest__.py
+++ pos_price_to_weight/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017-Today: La Louve (<http://www.lalouve.net/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Point of Sale - Price to Weight',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Point Of Sale',
     'summary': 'Compute weight based on barcodes with prices',
     'author': 'La Louve, GRAP, Odoo Community Association (OCA)',
