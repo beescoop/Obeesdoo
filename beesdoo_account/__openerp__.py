d20fbff1718f3c0331db0af8225c4e0b3c4c460a
Augustin Borsu <a.borsu@gmail.com>
2019-11-17 11:27:38 +0100

    beesdoo_account: migration to version 12.0

--- beesdoo_account/__openerp__.py
+++ beesdoo_account/__openerp__.py
@@ -1,22 +1,16 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Beescoop  Account Module",
-
-    'summary': """
+    "name": "Beescoop  Account Module",
+    "summary": """
 		Module that customize account module for Beescoop
      """,
-
-    'description': """
+    "description": """
+        Makes date_invoice field required in account.invoice_form and
+        account.invoice_supplier_form
     """,
-
-    'author': "Beescoop - Cellule IT",
-    'website': "https://github.com/beescoop/Obeesdoo",
-
-    'category': 'Account Module',
-    'version': '0.1',
-
-    'depends': ['account',
-                'beesdoo_base'],
-
-    'data': ['views/account_invoice.xml'],
+    "author": "Beescoop - Cellule IT",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Account Module",
+    "version": "12.0.1.0.0",
+    "depends": ["account", "beesdoo_base"],
+    "data": ["views/account_invoice.xml"],
 }
