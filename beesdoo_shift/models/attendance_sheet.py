4f2c3cee1b6ad6d6727da1179b5df5e62923863e
Elouan Le Bars <elouan@coopiteasy.be>
2019-12-20 12:22:37 +0100

    [FIX] b_shift : time on added shift

--- beesdoo_shift/models/attendance_sheet.py
+++ beesdoo_shift/models/attendance_sheet.py
@@ -489,7 +489,7 @@ class AttendanceSheet(models.Model):
             else:
                 actual_shift = self.env["beesdoo.shift.shift"].create(
                     {
-                        "name": _("[Added Shift] %s") % self.start_time,
+                        "name": _("[Added Shift]"),
                         "task_type_id": added_shift.task_type_id.id,
                         "state": added_shift.state,
                         "worker_id": added_shift.worker_id.id,
