e8d17f25b05a8180d00277bd5d8e267ada05a5a3
Elouan Le Bars <elouan@coopiteasy.be>
2019-12-24 11:16:56 +0100

    [ADD] beesdoo_pos_reporting

---
+++ beesdoo_pos_reporting/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019  Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Elouan LE BARS - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Beesdoo POS Reporting",
+    "summary": "Enhance POS with features allowing statistics and reporting.",
+    "description": "",
+    "version": "12.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "data": [],
+}
