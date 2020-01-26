258508494a7554f7256e3ad348faa465cb935fa8
Rémy Taymans <remytaymans@gmail.com>
2020-01-26 10:53:48 +0100

    [MIG] beesdoo_website_shift: Migrate to 12.0

--- beesdoo_website_shift/__manifest__.py
+++ beesdoo_website_shift/__manifest__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2017-2018 Rémy Taymans <remytaymans@gmail.com>
+# Copyright 2017-2020 Coop IT Easy (http://coopiteasy.be)
+#   Rémy Taymans <remy@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'BEES coop Website Shift',
-
     'summary': """
         Show available shifts for regular and irregular workers on the
         website and let workers manage their shifts with an
@@ -13,20 +11,16 @@
     """,
     'description': """
     """,
-
-    'author': 'Rémy Taymans',
-    'license': 'AGPL-3',
-    'version': '9.0.2.3.1',
-    'website': "https://github.com/beescoop/Obeesdoo",
-
-    'category': 'Cooperative management',
-
-    'depends': ['website', 'beesdoo_shift'],
-
-    'data': [
-        'data/res_config_data.xml',
-        'views/shift_website_templates.xml',
-        'views/my_shift_website_templates.xml',
-        'views/res_config_views.xml',
-    ]
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Cooperative management",
+    "depends": ["portal", "website", "beesdoo_shift"],
+    "data": [
+        "data/res_config_data.xml",
+        "views/shift_website_templates.xml",
+        "views/my_shift_website_templates.xml",
+        "views/res_config_views.xml",
+    ],
 }
--- beesdoo_website_shift/models/res_config.py
+++ beesdoo_website_shift/models/res_config.py
@@ -1,136 +1,45 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2017-2018 Rémy Taymans <remytaymans@gmail.com>
+# Copyright 2017-2020 Rémy Taymans <remytaymans@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from ast import literal_eval
 from odoo import fields, models, api
 
-PARAMS = [
-    ('irregular_shift_limit', 'beesdoo_website_shift.irregular_shift_limit'),
-    ('highlight_rule_pc', 'beesdoo_website_shift.highlight_rule_pc'),
-    ('hide_rule', 'beesdoo_website_shift.hide_rule'),
-    ('irregular_enable_sign_up',
-     'beesdoo_website_shift.irregular_enable_sign_up'),
-    ('irregular_past_shift_limit',
-     'beesdoo_website_shift.irregular_past_shift_limit'),
-    ('regular_past_shift_limit',
-     'beesdoo_website_shift.regular_past_shift_limit'),
-    ('regular_next_shift_limit',
-     'beesdoo_website_shift.regular_next_shift_limit'),
-    ('regular_highlight_rule',
-     'beesdoo_website_shift.regular_highlight_rule'),
-]
-
 
 class WebsiteShiftConfigSettings(models.TransientModel):
-    _name = 'beesdoo.website.shift.config.settings'
     _inherit = 'res.config.settings'
 
     # Irregular worker settings
     irregular_shift_limit = fields.Integer(
-        help="Maximum shift that will be shown"
+        related='website_id.irregular_shift_limit',
+        readonly=False,
     )
     highlight_rule_pc = fields.Integer(
-        help="Treshold (in %) of available space in a shift that trigger the "
-             "highlight of the shift"
+        related='website_id.highlight_rule_pc',
+        readonly=False,
     )
     hide_rule = fields.Integer(
-        help="Treshold ((available space)/(max space)) in percentage of "
-             "available space under wich the shift is hidden"
+        related='website_id.highlight_rule_pc',
+        readonly=False,
     )
     irregular_enable_sign_up = fields.Boolean(
-        help="Enable shift sign up for irregular worker"
+        related='website_id.irregular_enable_sign_up',
+        readonly=False,
     )
     irregular_past_shift_limit = fields.Integer(
-        help="Maximum past shift that will be shown for irregular worker"
+        related='website_id.irregular_past_shift_limit',
+        readonly=False,
     )
 
     # Regular worker settings
     regular_past_shift_limit = fields.Integer(
-        help="Maximum past shift that will be shown for regular worker"
+        related='website_id.regular_past_shift_limit',
+        readonly=False,
     )
     regular_next_shift_limit = fields.Integer(
-        help="Maximun number of next shift that will be shown"
+        related='website_id.regular_next_shift_limit',
+        readonly=False,
     )
     regular_highlight_rule = fields.Integer(
-        help="Treshold (in %) of available space in a shift that trigger the "
-        "the highlight of a shift template."
+        related='website_id.regular_highlight_rule',
+        readonly=False,
     )
-
-    @api.multi
-    def set_params(self):
-        self.ensure_one()
-
-        for field_name, key_name in PARAMS:
-            value = getattr(self, field_name)
-            self.env['ir.config_parameter'].set_param(key_name, str(value))
-
-    @api.multi
-    def get_default_irregular_shift_limit(self):
-        return {
-            'irregular_shift_limit': int(
-                self.env['ir.config_parameter']
-                .get_param("beesdoo_website_shift.irregular_shift_limit")
-            )
-        }
-
-    @api.multi
-    def get_default_highlight_rule_pc(self):
-        return {
-            'highlight_rule_pc': int(
-                self.env['ir.config_parameter']
-                .get_param("beesdoo_website_shift.highlight_rule_pc")
-            )
-        }
-
-    @api.multi
-    def get_default_hide_rule(self):
-        return {
-            'hide_rule': int(self.env['ir.config_parameter'].get_param(
-                'beesdoo_website_shift.hide_rule'))
-        }
-
-    @api.multi
-    def get_default_irregular_shift_sign_up(self):
-        return {
-            'irregular_enable_sign_up':
-                literal_eval(self.env['ir.config_parameter'].get_param(
-                    'beesdoo_website_shift.irregular_enable_sign_up'))
-        }
-
-    @api.multi
-    def get_default_irregular_past_shift_limit(self):
-        return {
-            'irregular_past_shift_limit': int(
-                self.env['ir.config_parameter']
-                .get_param("beesdoo_website_shift.irregular_past_shift_limit")
-            )
-        }
-
-    @api.multi
-    def get_default_regular_past_shift_limit(self):
-        return {
-            'regular_past_shift_limit': int(
-                self.env['ir.config_parameter']
-                .get_param('beesdoo_website_shift.regular_past_shift_limit')
-            )
-        }
-
-    @api.multi
-    def get_default_regular_next_shift_limit(self):
-        return {
-            'regular_next_shift_limit': int(
-                self.env['ir.config_parameter']
-                .get_param('beesdoo_website_shift.regular_next_shift_limit')
-            )
-        }
-
-    @api.multi
-    def get_default_regular_highlight_rule(self):
-        return {
-            'regular_highlight_rule': int(
-                self.env['ir.config_parameter']
-                .get_param('beesdoo_website_shift.regular_highlight_rule')
-            )
-        }
