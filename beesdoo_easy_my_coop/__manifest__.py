b1d07d6bf1381750c4d5c216d4f9449417fe5486
Cyrille Bollu <cyrpub@bollu.be>
2020-01-27 17:10:52 +0100

    [MIG] beesdoo_easy_my_coop: Migration to 12.0

--- beesdoo_easy_my_coop/__manifest__.py
+++ beesdoo_easy_my_coop/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Cooperative management',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['beesdoo_base', 'beesdoo_shift', 'easy_my_coop', 'easy_my_coop_eater'],
