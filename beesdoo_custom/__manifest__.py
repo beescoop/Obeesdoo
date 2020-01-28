1ee245f2dc0c91fcc95cd1c3fe66904f9ba0ddfc
Cyrille Bollu <cyrpub@bollu.be>
2020-01-28 11:59:22 +0100

    [MIG] beesdoo_custom: Migration to 11.0

--- beesdoo_custom/__manifest__.py
+++ beesdoo_custom/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "beesdoo_custom",
 
@@ -13,7 +12,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sales Management',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': [
