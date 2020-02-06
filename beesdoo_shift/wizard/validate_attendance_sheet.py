d163dba0c22f5f148d6e27b2b9406fcba6f97d21
Elouan Le Bars <elouan@coopiteasy.be>
2020-02-06 15:52:41 +0100

    [REF] b_shift : attendance sheet validation wizard

--- beesdoo_shift/wizard/validate_attendance_sheet.py
+++ beesdoo_shift/wizard/validate_attendance_sheet.py
@@ -68,7 +68,7 @@ class ValidateAttendanceSheet(models.TransientModel):
     notes = fields.Text(related="active_sheet.notes", default="", readonly=False)
     feedback = fields.Text(related="active_sheet.feedback", default="", readonly=False)
     worker_nb_feedback = fields.Selection(
-        related="active_sheet.worker_nb_feedback", readonly=False, required=True
+        related="active_sheet.worker_nb_feedback", readonly=False
     )
 
     def on_barcode_scanned(self, barcode):
@@ -84,6 +84,10 @@ class ValidateAttendanceSheet(models.TransientModel):
     @api.multi
     def validate_sheet(self):
         sheet = self.active_sheet
+
+        if not self.worker_nb_feedback:
+            raise UserError(_("Please give your feedback on the number of workers."))
+
         if self.card_support:
             # Login with barcode
             card = self.env["member.card"].search(
