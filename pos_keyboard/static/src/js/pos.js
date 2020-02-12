60c04165470a02ca0715fc672fea9b88b032b0b3
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-12 09:34:32 +0000

    :green_heart: pre-commit: auto cleanups

--- pos_keyboard/static/src/js/pos.js
+++ pos_keyboard/static/src/js/pos.js
@@ -3,6 +3,7 @@
     Copyright 2016 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
     Copyright 2019 Kolushov Alexandr <https://it-projects.info/team/kolushovalexandr>
     Copyright 2019 ssaid <https://github.com/ssaid>
+    Copyright 2019 raulovallet <https://github.com/raulovallet>
     License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html). */
 /* eslint-disable complexity */
 odoo.define('pos_keyboard.pos', function (require) {
@@ -248,7 +249,7 @@ self.action_callback(self.data);
         // stops catching keyboard events
         disconnect: function(){
             $('body').off('keyup', '');
-            self.active = false;
+            this.active = false;
         }
     });
 
