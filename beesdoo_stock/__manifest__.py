5ae8aa23ef383145ba3fda01d15f2598eeb555f0
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-15 11:17:22 +0200

    [MIG] beesdoo_stock: migration to 11.0 - followed oca guidelines - not tested on odoo 11.0

--- beesdoo_stock/__manifest__.py
+++ beesdoo_stock/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRLfs
 # Nicolas Jamoulle, <nicolas@coopiteasy.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "BEES coop Stock",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         'stock',
     ],
