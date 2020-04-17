e85f9f199d4c342668b5f8a1789653185c01ee54
Houssine BAKKALI <houssine.bakkali@gmail.com>
2020-04-17 11:38:57 +0200

    [FIX] fix missing underscore for _inherit

--- beesdoo_easy_my_coop/models/product.py
+++ beesdoo_easy_my_coop/models/product.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 
 class ProductTemplate(models.Model):
 
-    inherit = 'product.template'
+    _inherit = 'product.template'
 
     can_shop = fields.Boolean(string="Is share?")
