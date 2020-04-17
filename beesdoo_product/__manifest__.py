7fc327ecbb2cb2cd95e0b6dab757881f89c4362f
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-04-17 17:14:48 +0200

    [REM] pos_price_to_weight: remove in favour of OCA version

--- beesdoo_product/__manifest__.py
+++ beesdoo_product/__manifest__.py
@@ -6,29 +6,31 @@
 #   - Thibault François
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "beesdoo_product",
-
-    'summary': """
+    "name": "beesdoo_product",
+    "summary": """
         Modification of product module for the needs of beescoop
         - SOOO5 - Ajout de label bio/ethique/provenance""",
-
-    'description': """
+    "description": """
 
     """,
-
-    'author': "Beescoop - Cellule IT",
-    'website': "https://github.com/beescoop/Obeesdoo",
-    'category': 'Sales',
-    'version': '12.0.1.0.0',
-    'depends': ['beesdoo_base', 'product', 'sale', 'point_of_sale', 'pos_price_to_weight'],
-
-    'data': [
-        'data/product_label.xml',
-        'data/barcode_rule.xml',
-        'data/product_sequence.xml',
-        'views/beesdoo_product.xml',
-        'wizard/views/label_printing_utils.xml',
-        'security/ir.model.access.csv',
+    "author": "Beescoop - Cellule IT",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Sales",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "beesdoo_base",
+        "product",
+        "sale",
+        "point_of_sale",
+        "pos_price_to_weight",  # OCA
     ],
-    'installable': True,
+    "data": [
+        "data/product_label.xml",
+        "data/barcode_rule.xml",
+        "data/product_sequence.xml",
+        "views/beesdoo_product.xml",
+        "wizard/views/label_printing_utils.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
 }
