1c9fe6395c5766e6c23a437194471a55b0eb4d52
Rémy Taymans <remytaymans@gmail.com>
2020-05-06 22:25:45 +0200

    [IMP] b_custom: manifest

--- beesdoo_custom/__manifest__.py
+++ beesdoo_custom/__manifest__.py
@@ -1,28 +1,22 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "beesdoo_custom",
+    'name': "BEES coop Custom",
 
     'description': """
-        View and field definition specific to BEES' needs.   
+        View and field definition specific to BEES' needs.
      """,
 
-    'author': "Beescoop - Cellule IT",
+    'author': "Beescoop - Cellule IT, Coop IT Easy",
     'website': "https://github.com/beescoop/Obeesdoo",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Sales Management',
     'version': '12.0.1.0.0',
-
-    # any module necessary for this one to work correctly
     'depends': [
         'beesdoo_product',
         'beesdoo_stock_coverage',
         'purchase',
-        'easy_my_coop', # for product views
+        'easy_my_coop',  # for product views
     ],
-
-    # always loaded
     'data': [
         'views/beesdoo_product.xml',
     ],
