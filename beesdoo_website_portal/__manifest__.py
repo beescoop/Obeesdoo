5d75b53dad3eff62a73dffd1d17c754555685a56
Vincent Van Rossem <vvrossem@gmail.com>
2020-06-08 18:46:28 +0200

    [MIG] beesdoo_website_portal: 10.0 -> 11.0 - followed main oca guidelines - not tested on odoo 11.0

--- beesdoo_website_portal/__manifest__.py
+++ beesdoo_website_portal/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 - Today Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
 # - Rémy Taymans  <remy@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -10,7 +9,7 @@
     """,
     'author': 'Coop IT Easy SCRLfs',
     'license': 'AGPL-3',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'website': "https://www.coopiteasy.be",
     'category': 'Cooperative management',
     'depends': [
