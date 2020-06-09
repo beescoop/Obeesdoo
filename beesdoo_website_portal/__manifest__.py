2fec347d8cd108638815e8a09e984d2e948e4b23
Vincent Van Rossem <vvrossem@gmail.com>
2020-06-09 09:44:41 +0200

    [MIG] beesdoo_website_portal: 11.0 -> 12.0

--- beesdoo_website_portal/__manifest__.py
+++ beesdoo_website_portal/__manifest__.py
@@ -9,7 +9,7 @@
     """,
     'author': 'Coop IT Easy SCRLfs',
     'license': 'AGPL-3',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'website': "https://www.coopiteasy.be",
     'category': 'Cooperative management',
     'depends': [
