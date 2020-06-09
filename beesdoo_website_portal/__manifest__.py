748e198ba0ef2865b641fc9febfd0b15a5595ee4
Vincent Van Rossem <vvrossem@gmail.com>
2020-06-09 11:36:47 +0200

    [IMP] beesdoo_website_portal: black formatter

--- beesdoo_website_portal/__manifest__.py
+++ beesdoo_website_portal/__manifest__.py
@@ -3,21 +3,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'BEES coop Website Portal',
-    'summary': """
+    "name": "BEES coop Website Portal",
+    "summary": """
     Extension of the Portal that prevent modification of sensible data by the users
     """,
-    'author': 'Coop IT Easy SCRLfs',
-    'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
-    'website': "https://www.coopiteasy.be",
-    'category': 'Cooperative management',
-    'depends': [
-        'portal',
-        'website'
-    ],
-    'data': [
-        'views/portal_website_templates.xml',
-    ],
-    'installable': True,
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "website": "https://www.coopiteasy.be",
+    "category": "Cooperative management",
+    "depends": ["portal", "website"],
+    "data": ["views/portal_website_templates.xml",],
+    "installable": True,
 }
