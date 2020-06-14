89a00bb06ac839387293abf8e702f6f1c9163820
robin.keunen <robin@keunen.net>
2020-06-14 15:24:29 +0200

    [REF] blacken

--- beesdoo_product_usability/models/beesdoo_product.py
+++ beesdoo_product_usability/models/beesdoo_product.py
@@ -1,25 +1,23 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class BeesdooProduct(models.Model):
     _inherit = "product.template"
 
     main_supplierinfo = fields.Many2one(
-        'product.supplierinfo',
-        string='Main Supplier Information',
-        compute='_compute_main_supplierinfo'
+        "product.supplierinfo",
+        string="Main Supplier Information",
+        compute="_compute_main_supplierinfo",
     )
     main_price = fields.Float(
-        string='Price',
-        compute='_compute_main_supplierinfo',
+        string="Price", compute="_compute_main_supplierinfo"
     )
     main_minimum_qty = fields.Float(
-        string='Minimum Quantity',
-        compute='_compute_main_supplierinfo',
+        string="Minimum Quantity", compute="_compute_main_supplierinfo"
     )
 
     @api.multi
-    @api.depends('seller_ids')
+    @api.depends("seller_ids")
     def _compute_main_supplierinfo(self):
         for product in self:
             supplierinfo = product._get_main_supplier_info()
