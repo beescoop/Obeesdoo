5922e753e8d059dfadc2cb8d8e9b2fd188e6f03d
Rémy Taymans <remytms@tsmail.eu>
2020-11-09 17:41:17 +0100

    [FIX] b_w_shift: Past shift alert message

--- beesdoo_website_shift/__manifest__.py
+++ beesdoo_website_shift/__manifest__.py
@@ -11,7 +11,7 @@
     """,
     "author": "Coop IT Easy SCRLfs",
     "license": "AGPL-3",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Cooperative management",
     "depends": ["portal", "website", "beesdoo_shift"],
