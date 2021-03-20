1ff72cf6f17d890fd20c4318bda4b9ad8fed87b9
houssine <houssine.bakkali@gmail.com>
2021-03-20 15:11:33 +0100

    [IMP] define specific group for readonly access for on products.
    This also give access to the backend without giving all the rights of
    the user (employee group)

--- beesdoo_product_info_screen/__manifest__.py
+++ beesdoo_product_info_screen/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2021 Coop IT Easy SCRL fs
 #   Robin Keunen <robin@coopiteasy.be>
+#   Houssine Bakkali <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Information Screen",
