44248e8a69c66039d5cd2ce97c718c8a99dff16d
eliotvangoethem <eliot.vangoethem@gmail.com>
2021-03-22 13:50:44 +0100

    [MODIF] variable name

--- beesdoo_shift_swap/models/exchange_proposale.py
+++ beesdoo_shift_swap/models/exchange_proposale.py
@@ -1,19 +1,19 @@
 from odoo import models, fields, api
 
 class exchange_proposale(models.Model):
-    _name = 'beesdoo_bourse_shift.exchange_proposale'
+    _name = 'beesdoo.shift.exchange_proposale'
 
 
     asked_timeslot_ids = fields.One2many(
-        comodel_name='beesdoo_bourse_shift.exchange_proposale',
+        comodel_name='beesdoo.shift.exchange_proposale',
         inverse_name='id',
         string='asked_timeslots'
     )
-    exchanged_timeslot_id = fields.Many2one('beesdoo_bourse_shift.timeslots_date', string='exchanged_timeslot')
+    exchanged_timeslot_id = fields.Many2one('beesdoo.shift.timeslots_date', string='exchanged_timeslot')
     proposition_date = fields.Date(required = True)
-    exchange_set_id = fields.Many2one('beesdoo_bourse_shift.exchange_set',string = 'exchanged_set')
+    exchange_set_id = fields.Many2one('beesdoo.shift.exchange_set',string = 'exchanged_set')
     validate_proposale_id = fields.One2many(
-        comodel_name='beesdoo_bourse_shift.exchange_proposale',
+        comodel_name='beesdoo.shift.exchange_proposale',
         inverse_name='id',
         string='validate_proposale'
     )
--- beesdoo_shift_swap/models/subscribed_underpopulated_shift.py
+++ beesdoo_shift_swap/models/subscribed_underpopulated_shift.py
@@ -1,18 +1,20 @@
 from odoo import models, fields, api
 
 class subscribe_underpopulated_shift(models.Model):
-    _name = 'beesdoo_bourse_shift.subscribed_underpopulated_shift'
+    _name = 'beesdoo.shift.subscribed_underpopulated_shift'
 
     exchanged_timeslot_id = fields.One2many(
-        comodel_name='beesdoo_bourse_shift.timeslots_date',
+        comodel_name='beesdoo.shift.timeslots_date',
         inverse_name='id',
         string='exchanged_shift'
     )
     comfirmed_timeslot_id = fields.One2many(
-        comodel_name='beesdoo_bourse_shift.timeslots_date',
+        comodel_name='beesdoo.shift.timeslots_date',
         inverse_name='id',
         string='asked_shift'
     )
     date = fields.Date(required = True)
     status = fields.Char(compute = 'compute_status')
+
+
 
