d658a2610e7cd9f7f7813d285155d6cad6be2913
eliotvangoethem <eliot.vangoethem@gmail.com>
2021-03-25 10:38:24 +0100

    [ADD] different access right

--- beesdoo_shift_swap/models/exchange_set.py
+++ beesdoo_shift_swap/models/exchange_set.py
@@ -7,12 +7,12 @@ class exchange_set(models.Model):
     second_shift = fields.Many2one('beesdoo.shift.shift', string='second_shift')
     first_proposale_id=fields.One2many(
         comodel_name='beesdoo.shift.exchange_proposale',
-        inverse_name='exchanged_set_id',
+        inverse_name='exchange_set_id',
         string='first_proposale'
     )
     second_proposale_id= fields.One2many(
         comodel_name='beesdoo.shift.exchange_proposale',
-        inverse_name='exchanged_set_id',
+        inverse_name='exchange_set_id',
         string='second_proposale'
     )
     status_generated=fields.Selection(['exchanged_generated', 'first_shift_not_generated', 'second_shift_not_generated', 'both_not_generated'])
--- beesdoo_shift_swap/models/timeslots_date.py
+++ beesdoo_shift_swap/models/timeslots_date.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class TimeslotsDate(models.Model):
     _name = 'beesdoo.shift.timeslots_date'
 
-    date = fields.Date(required = True)
+    date = fields.Datetime(required = True)
     template_id = fields.Many2one("beesdoo.shift.template")
     shift_id = fields.Integer(compute='compute_shift')
 
