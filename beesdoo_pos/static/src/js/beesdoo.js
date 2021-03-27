b8c1465738b1fbf8512f51962cc1c21e55a6fbfe
houssine <houssine.bakkali@gmail.com>
2021-03-27 18:50:13 +0100

    [IMP] add autoselect invoice when customer is company

--- beesdoo_pos/static/src/js/beesdoo.js
+++ beesdoo_pos/static/src/js/beesdoo.js
@@ -1,6 +1,10 @@
 odoo.define('beesdoo_pos.screens', function (require) {
     "use strict";
     var screens = require("point_of_sale.screens");
+    var models = require('point_of_sale.models');
+
+    models.load_fields('res.partner', 'is_company');
+
     var set_customer_info = function(el_class, value, prefix) {
         var el = this.$(el_class);
         el.empty();
@@ -67,12 +71,29 @@ odoo.define('beesdoo_pos.screens', function (require) {
                     loaded.reject(err);
                 });
         },
+
+        auto_invoice : function() {
+        	var self = this;
+        	var customer = this.pos.get_client();
+        	var order = this.pos.get_order();
+        	if (customer && customer.is_company && !order.is_to_invoice()){
+            	self.click_invoice();
+        	}
+        	if (!customer && order.is_to_invoice()){
+        		self.click_invoice();
+        	}
+        },
+
         renderElement : function() {
             this._super();
             this.render_customer_info();
         },
+        
         customer_changed : function() {
             this._super();
+            if (this.pos.config.module_account){
+            	this.auto_invoice();
+            }
             this.render_customer_info();
         },
     });
