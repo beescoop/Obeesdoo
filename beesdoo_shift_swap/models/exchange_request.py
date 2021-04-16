7ea8b7cdeadc8598f40833c6acb499729560e90f
eliotvangoethem <eliot.vangoethem@gmail.com>
2021-04-16 17:20:47 +0200

    [ADD] function display_timeslot, can generate timeslot of shift already generated

--- beesdoo_shift_swap/models/exchange_request.py
+++ beesdoo_shift_swap/models/exchange_request.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-class exchange_proposale(models.Model):
+class ExchangeRequest(models.Model):
     _name = 'beesdoo.shift.exchange_proposale'
 
     worker_id = fields.Many2one(
@@ -10,6 +10,7 @@ class exchange_proposale(models.Model):
             ("working_mode", "in", ("regular", "irregular")),
             ("state", "not in", ("unsubscribed", "resigning")),
         ],
+        string="worker"
     )
     asked_timeslot_ids = fields.One2many(
         comodel_name='beesdoo.shift.timeslots_date',
@@ -17,40 +18,36 @@ class exchange_proposale(models.Model):
         string='asked_timeslots'
     )
     exchanged_timeslot_id = fields.Many2one('beesdoo.shift.timeslots_date', string='exchanged_timeslot')
-    proposition_date = fields.Date(required = True)
+    proposition_date = fields.Date(required = True, string='date')
     exchange_set_id = fields.Many2one('beesdoo.shift.exchange_set',string = 'exchanged_set')
     validate_proposale_id = fields.One2many(
         comodel_name='beesdoo.shift.exchange_proposale',
         inverse_name='id',
         string='validate_proposale'
     )
-    status = fields.Char(compute='compute_status')
+    def _get_status(self):
+        return [
+            ('draft', 'Draft'),
+            ('validate', 'Validate'),
+            ('done', 'Done')
+        ]
+    status = fields.Selection(
+        selection=_get_status,
+        default='draft'
+    )
 
-    @api.multi
-    def is_match(self):
-        worker = self.worker_id
-        my_timeslot = self.exchanged_timeslot_id
+    def matching_request(self):
         timeslot_wanted = self.asked_timeslot_ids
-        my_match = []
+        matches = self.env["beesdoo.shift.exchange.proposal"]  # Creates an empty recordset for proposals
 
         for rec in timeslot_wanted :
             match_exchange_rec = self.env["beesdoo.shift.exchange_proposale"]\
                 .search([
-                ("exchanged_timeslot_ids",'=', rec.id),
+                ("exchanged_timeslot_id",'=', rec.id),
             ])
-            my_match.append(match_exchange_rec)
-        if len(my_match)>0 :
-            return True
-        return False
+            matches |= match_exchange_rec
+        return matches
 
-    @api.multi
-    def button_is_match(self):
-        for proposale in self :
-            if not proposale.asked_timeslot_ids:
-                raise Warning('Please provide wanted timeslot')
-            if not proposale.is_match() :
-                raise Warning('no match')
-        return True
 
 
 
--- beesdoo_shift_swap/models/timeslots_date.py
+++ beesdoo_shift_swap/models/timeslots_date.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 class TimeslotsDate(models.Model):
     _name = 'beesdoo.shift.timeslots_date'
@@ -12,6 +13,83 @@ class TimeslotsDate(models.Model):
         timeslot= self.env["beesdoo.shift.timeslots_date"].create()
         timeslot.date = shift.start_time
         timeslot.template_id = shift.task_template_id
+
+    @api.model
+    def display_timeslot(self,my_timeslot):
+        start_date = datetime.now()
+        timeslot_rec = []
+        template = self.env["beesdoo.shift.template"].search([])
+
+        #generate timeslot of the shift already generated
+        shift_generated = (self.env["beesdoo.shift.shift"].sudo().search([
+            ("start_time", ">", start_date.strftime("%Y-%m-%d %H:%M:%S"))
+        ],
+        order="start_time, task_template_id, task_type_id"))
+        first_shift = shift_generated[0]
+        last_template = first_shift.task_template_id
+        new_template = first_shift.task_template_id
+
+        first_timeslot = self.env["beesdoo.shift.timeslots_date"].new()
+        first_timeslot.template_id = first_shift.task_template_id
+        first_timeslot.date = first_shift.start_time
+        timeslot_rec.append((first_timeslot.template_id.name,first_timeslot.date))
+
+
+        shift_generated_list = []
+        for shift_rec in shift_generated :
+            shift_generated_list.append(shift_rec)
+
+        for i in range(0,len(shift_generated_list)) :
+            if last_template != new_template :
+                timeslot = self.env["beesdoo.shift.timeslots_date"].new()
+                timeslot.template_id = shift_generated_list[i].task_template_id
+                timeslot.date = shift_generated_list[i].start_time
+                timeslot_rec.append((timeslot.template_id.name,timeslot.date))
+                new_template = shift_generated_list[i].task_template_id
+            last_template = shift_generated_list[i].task_template_id
+
+        #return timeslot_rec
+
+        #now we have to generate timeslot of the shift that aren't generated
+
+        #Get parameters
+
+        #TODO : add system parameters of +2months
+        shift_period = int(
+            self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("beesdoo_website_shift.shift_period")
+        )
+        end_date = my_timeslot.date + timedelta(days=2*shift_period)
+        last_sequence = int(
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("last_planning_seq")
+        )
+        #change 4 en allant chercher le nombre de planning
+        #utiliser get_next_planning
+        next_planning_nb = (last_sequence % 4) + 1
+        next_planning = self.env["beesdoo.shift.planning"].search([
+            ("sequence" , "=" , next_planning_nb)
+        ])
+        #TODO :check if we have one records for last_planning
+        templates = self.env["beesdoo.shift.template"].search(
+            [
+            ("planning_id","=",next_planning.id),
+            ],
+            order="day_nb_id" ,
+        )
+
+        next_planning_date = datetime.strptime(
+            self.env["ir.config_parameter"].sudo().get_param("next_planning_date"),
+            '%Y-%m-%d'
+        )
+
+        return timeslot_rec
+
+
+
+
 
 
     
