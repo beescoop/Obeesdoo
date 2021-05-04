23e04fb2d911b228c7f335d5a18d6a5ee9aba998
eliotvangoethem <eliot.vangoethem@gmail.com>
2021-05-04 14:19:15 +0200

    [MODIf] renommage exchange_proposale by exchange_request

--- beesdoo_shift_swap/models/exchange.py
+++ beesdoo_shift_swap/models/exchange.py
@@ -5,15 +5,15 @@ class Exchange(models.Model):
 
     first_shift = fields.Many2one('beesdoo.shift.shift', string='first_shift')
     second_shift = fields.Many2one('beesdoo.shift.shift', string='second_shift')
-    first_proposale_id=fields.One2many(
-        comodel_name='beesdoo.shift.exchange_proposale',
+    first_request_id=fields.One2many(
+        comodel_name='beesdoo.shift.exchange_request',
         inverse_name='exchange_set_id',
-        string='first_proposale'
+        string='first_request'
     )
-    second_proposale_id= fields.One2many(
-        comodel_name='beesdoo.shift.exchange_proposale',
+    second_request_id= fields.One2many(
+        comodel_name='beesdoo.shift.exchange_request',
         inverse_name='exchange_set_id',
-        string='second_proposale'
+        string='second_request'
     )
     status_generated=fields.Selection([('exchanged_generated','0'), ('first_shift_not_generated','1'), ('second_shift_not_generated','2'), ('both_not_generated','3')])
 
