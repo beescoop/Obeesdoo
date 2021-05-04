4fe50ef86d45253205c2630d857e8c1c881738b1
eliotvangoethem <eliot.vangoethem@gmail.com>
2021-05-04 14:18:25 +0200

    [ADD] front end view

--- beesdoo_website_shift_swap/controllers/controllers.py
+++ beesdoo_website_shift_swap/controllers/controllers.py
@@ -15,6 +15,19 @@ from odoo.http import request
 
 class WebsiteShiftSwapController(http.Controller):
 
-    @http.route("/my/shift/choose/swap")
-    def display_shift(self):
-     return('<h1>yolo</h1>')
+    @http.route("/my/shift/<string:my_shift>/choose/swap")
+    def my_available_shift(self,my_shift):
+        """
+        Personal page for swaping your shifts
+        :return:
+        """
+        my_available_shift = (
+            request.env["beesdoo.shift.subscribed_underpopulated_shift"]
+            .sudo()
+            .display_underpopulated_shift(my_shift)
+        )
+        return request.render ("beesdoo_website_shift_swap.website_shift_swap_swap",
+            {
+            "available_shift" : my_available_shift
+            }
+        )
