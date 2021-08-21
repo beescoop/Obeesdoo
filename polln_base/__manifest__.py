0aac4cb94d4bb7bf1a441d0d33dbc7a9a98d6fdd
odoo-pda <pda@odoo.com>
2021-08-21 18:23:58 +0200

    [ADD] polln_base, card members report.

---
+++ polln_base/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Polln
+#   - Patricia Daloze
+#   - Thibault François
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Polln Base Module",
+    "summary": """
+    Module that customize the base module and contains some python tools
+     """,
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Sales",
+    "version": "12.0.1.0.1",
+    "depends": ["beesdoo_base"],
+    "data": [
+        "report/pollncard.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
