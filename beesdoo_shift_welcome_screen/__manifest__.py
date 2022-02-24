a2b1e60714a0b79a4a7c2c9ebea279737a9c2d52
Thibault Francois <francois.th@gmail.com>
2022-02-24 16:04:16 +0100

    [ADD] beesdoo_shift_welcome_screen: new module
    Add a module for cooperator welcome screen
    
    - Scan your member card
    - See if you can shop
    - See your counter and status
    
    - See your eater
    - See your next shift

---
+++ beesdoo_shift_welcome_screen/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Beescoop Shift Management - Welcome Screen",
+
+    'summary': """
+        Volonteer Timetable Management""",
+
+    'description': """
+
+    """,
+
+    'author': "Thibault Francois",
+    'website': "https://github.com/beescoop/Obeesdoo",
+
+    'category': 'Cooperative management',
+    'version': '0.1',
+
+    'depends': ['beesdoo_shift', 'barcodes'],
+
+    'data': [
+        "views/welcome.xml",
+    ],
+}
