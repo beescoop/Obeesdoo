2e9c3d02426fc78c07034a48432b4b4c2f428159
robin.keunen <robin@keunen.net>
2022-02-24 16:51:11 +0100

    [REF] beesdoo_shift_welcome: pre-commit

--- beesdoo_shift_welcome_screen/__manifest__.py
+++ beesdoo_shift_welcome_screen/__manifest__.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Thibault François
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
-    'name': "Beescoop Shift Management - Welcome Screen",
-
-    'summary': """
-        Volonteer Timetable Management""",
-
-    'description': """
-
-    """,
-
-    'author': "Thibault Francois",
-    'website': "https://github.com/beescoop/Obeesdoo",
-
-    'category': 'Cooperative management',
-    'version': '0.1',
-
-    'depends': ['beesdoo_shift', 'barcodes'],
-
-    'data': [
-        "views/welcome.xml",
-    ],
+    "name": "Beescoop Shift Management - Welcome Screen",
+    "summary": """
+        Volunteer Timetable Management""",
+    "author": "Thibault Francois",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Cooperative management",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["beesdoo_shift", "barcodes"],
+    "data": ["views/welcome.xml"],
 }
--- beesdoo_shift_welcome_screen/models/welcome.py
+++ beesdoo_shift_welcome_screen/models/welcome.py
@@ -1,29 +1,34 @@
-# -*- coding: utf-8 -*-
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Thibault François
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from datetime import datetime
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo import api, fields, models
+
 
 class TaskType(models.TransientModel):
-    _name = 'beesdoo.shift.welcome'
+    _name = "beesdoo.shift.welcome"
 
-    _inherit = ['barcodes.barcode_events_mixin']
+    _inherit = ["barcodes.barcode_events_mixin"]
 
-    partner_id = fields.Many2one('res.partner', string="Cooperator")
+    partner_id = fields.Many2one("res.partner", string="Cooperator")
     message = fields.Html("Message")
     # can_shop = fields.Boolean(related='partner_id.can_shop', readonly=True)
 
     def on_barcode_scanned(self, barcode):
-        self._barcode_scanned = ''
-        self.message = ''
+        self._barcode_scanned = ""
+        self.message = ""
 
-        if barcode.startswith('42'):
-            barcode = '0' + barcode
-        if not barcode.startswith('042'):
-            self.message = 'invalid barcode'
+        if barcode.startswith("42"):
+            barcode = "0" + barcode
+        if not barcode.startswith("042"):
+            self.message = "invalid barcode"
             return
-        #0 at the begining of the code bar seems not to be scanned
-        partner_ids = self.env['res.partner'].search([('barcode', '=', barcode)])
+        # 0 at the begining of the code bar seems not to be scanned
+        partner_ids = self.env["res.partner"].search(
+            [("barcode", "=", barcode)]
+        )
         if not partner_ids:
             self.message = "Member does not exist"
         elif len(partner_ids) > 1:
@@ -31,23 +36,30 @@ class TaskType(models.TransientModel):
         else:
             self.partner_id = partner_ids[0]
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def _onchange_partner(self):
         partner = self.partner_id
-        partner = partner.parent_eater_id if partner.parent_eater_id else partner
+        partner = (
+            partner.parent_eater_id if partner.parent_eater_id else partner
+        )
         values = {
-            'rec': self,
-            'partner': partner,
-            'next_shift': self._get_next_shift(partner),
+            "rec": self,
+            "partner": partner,
+            "next_shift": self._get_next_shift(partner),
         }
-        html_res = self.env.ref("beesdoo_shift_welcome_screen.welcome_message").render(values)
+        html_res = self.env.ref(
+            "beesdoo_shift_welcome_screen.welcome_message"
+        ).render(values)
         self.message = html_res
 
     def _get_next_shift(self, partner):
         if not partner:
             return self.env["beesdoo.shift.shift"]
         now = datetime.now()
-        return self.env["beesdoo.shift.shift"].search([
+        return self.env["beesdoo.shift.shift"].search(
+            [
                 ("start_time", ">", now.strftime("%Y-%m-%d %H:%M:%S")),
                 ("worker_id", "=", partner.id),
-            ], order="start_time, task_template_id, task_type_id")
+            ],
+            order="start_time, task_template_id, task_type_id",
+        )
