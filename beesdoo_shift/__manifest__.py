9b9f4631070eb6f07bc04441c8e9ea1ba3079800
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-02-28 14:37:10 +0100

    [FIX] beesdoo_shift: Depend on beesdoo_base
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- beesdoo_shift/__manifest__.py
+++ beesdoo_shift/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Cooperative management",
     "version": "12.0.1.1.0",
-    "depends": ["mail"],
+    "depends": ["beesdoo_base", "mail"],
     "data": [
         "data/system_parameter.xml",
         "data/cron.xml",
