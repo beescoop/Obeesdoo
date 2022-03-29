b8c7adc960b6d7258162a88be269948650ce3928
robin.keunen <robin@keunen.net>
2022-03-29 15:30:11 +0200

    [IMP] beesdoo_shift_welcome_screen: translate message

--- beesdoo_shift_welcome_screen/models/welcome.py
+++ beesdoo_shift_welcome_screen/models/welcome.py
@@ -4,7 +4,7 @@
 
 from datetime import datetime
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class TaskType(models.TransientModel):
@@ -14,7 +14,6 @@ class TaskType(models.TransientModel):
 
     partner_id = fields.Many2one("res.partner", string="Cooperator")
     message = fields.Html("Message")
-    # can_shop = fields.Boolean(related='partner_id.can_shop', readonly=True)
 
     def on_barcode_scanned(self, barcode):
         self._barcode_scanned = ""
@@ -23,33 +22,29 @@ class TaskType(models.TransientModel):
         if barcode.startswith("42"):
             barcode = "0" + barcode
         if not barcode.startswith("042"):
-            self.message = "invalid barcode"
+            self.message = _("Invalid barcode")
             return
         # 0 at the begining of the code bar seems not to be scanned
-        partner_ids = self.env["res.partner"].search(
-            [("barcode", "=", barcode)]
-        )
+        partner_ids = self.env["res.partner"].search([("barcode", "=", barcode)])
         if not partner_ids:
-            self.message = "Member does not exist"
+            self.message = _("Member does not exist")
         elif len(partner_ids) > 1:
-            self.message = "more then one member found with this barcode"
+            self.message = _("More then one member found with this barcode")
         else:
             self.partner_id = partner_ids[0]
 
     @api.onchange("partner_id")
     def _onchange_partner(self):
         partner = self.partner_id
-        partner = (
-            partner.parent_eater_id if partner.parent_eater_id else partner
-        )
+        partner = partner.parent_eater_id if partner.parent_eater_id else partner
         values = {
             "rec": self,
             "partner": partner,
             "next_shift": self._get_next_shift(partner),
         }
-        html_res = self.env.ref(
-            "beesdoo_shift_welcome_screen.welcome_message"
-        ).render(values)
+        html_res = self.env.ref("beesdoo_shift_welcome_screen.welcome_message").render(
+            values
+        )
         self.message = html_res
 
     def _get_next_shift(self, partner):
