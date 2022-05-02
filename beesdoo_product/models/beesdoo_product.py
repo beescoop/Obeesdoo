4eff285368b03b33d9430d471fa5548f0ff38535
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-05-02 10:10:06 +0200

    [ADD] beesdoo_product: Toggle for auto suggested price behaviour
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- beesdoo_product/models/beesdoo_product.py
+++ beesdoo_product/models/beesdoo_product.py
@@ -341,25 +341,28 @@ class BeesdooProduct(models.Model):
             product.purchase_price_write_date = supplierinfo.price_write_date
 
     def write(self, vals):
-        purchase_price = vals.get("purchase_price")
-        if purchase_price and purchase_price != self.purchase_price:
-            # force update of purchase_price (which actually modifies the
-            # suppliers’ price) to ensure that the next computations that
-            # depend on it are correct. there are 3 things to do:
-            # 1. set value in cache
-            with self.env.do_in_draft():
-                self.purchase_price = purchase_price
-            # 2. call inverse compute method
-            self._inverse_purchase_price()
-            # 3. remove the value from vals, to avoid the process to happen a
-            #    second time
-            del vals["purchase_price"]
+        if self.env["ir.config_parameter"].get_param(
+            "beesdoo_product.auto_write_suggested_price"
+        ):
+            purchase_price = vals.get("purchase_price")
+            if purchase_price and purchase_price != self.purchase_price:
+                # force update of purchase_price (which actually modifies the
+                # suppliers’ price) to ensure that the next computations that
+                # depend on it are correct. there are 3 things to do:
+                # 1. set value in cache
+                with self.env.do_in_draft():
+                    self.purchase_price = purchase_price
+                # 2. call inverse compute method
+                self._inverse_purchase_price()
+                # 3. remove the value from vals, to avoid the process to happen a
+                #    second time
+                del vals["purchase_price"]
 
-            # Important note: The list price is _only_ changed here if the
-            # `purchase_price` field of the product is changed. If the
-            # `price` field of the supplierinfo changes normally, the list price
-            # here is NOT automatically affected.
-            self.adapt_list_price(vals)
+                # Important note: The list price is _only_ changed here if the
+                # `purchase_price` field of the product is changed. If the
+                # `price` field of the supplierinfo changes normally, the list price
+                # here is NOT automatically affected.
+                self.adapt_list_price(vals)
 
         list_price = vals.get("list_price")
         if list_price and list_price != self.list_price:
