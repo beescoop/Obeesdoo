4892987424ed67d8ef1633bc952c0a2fd281a4f5
Rémy Taymans <remytms@tsmail.eu>
2022-05-26 17:36:12 +0200

    beesdoo_shift 12.0.1.1.4

--- beesdoo_shift/__manifest__.py
+++ beesdoo_shift/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Thibault Francois, Elouan Le Bars, Coop IT Easy SCRLfs",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Cooperative management",
-    "version": "12.0.1.1.3",
+    "version": "12.0.1.1.4",
     "depends": ["mail"],
     "data": [
         "data/system_parameter.xml",
