aca72c8a5570d4278cfb51c5e6821b2e862ab33f
MatteoLdx <com.mldx@ik.me>
2022-06-20 13:16:37 +0200

    [CHG] b_w_shift: unsubscribe from shift and (un)subscribe to compensation shift

---
+++ beesdoo_website_shift/static/src/js/script.js
@@ -0,0 +1,25 @@
+odoo.define("beesdoo_website_shift.script", function (require) {
+    "use strict";
+    $(document).ready(function () {
+        if ($(window).width() < 992) {
+            $("#collapseHelp").removeClass("show");
+            $("#toggle_help_button").text("Show");
+        }
+
+        $("#collapseHelp").on("hidden.bs.collapse", function () {
+            $("#toggle_help_button").text("Show");
+        });
+
+        $("#collapseHelp").on("shown.bs.collapse", function () {
+            $("#toggle_help_button").text("Hide");
+        });
+
+        $(".multi-collapse").on("hidden.bs.collapse", function () {
+            $("#toggle_shifts_button").text("Show next shifts");
+        });
+
+        $(".multi-collapse").on("shown.bs.collapse", function () {
+            $("#toggle_shifts_button").text("Reduce");
+        });
+    });
+});
