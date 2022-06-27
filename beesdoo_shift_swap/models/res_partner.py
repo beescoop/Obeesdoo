8c7c2449176173ca558166d4de7e60d4570dafc6
MatteoLdx <com.mldx@ik.me>
2022-06-27 15:35:14 +0200

    [IMP] b_shift,b_shift_swap: faster next shifts calculation

--- beesdoo_shift_swap/models/res_partner.py
+++ beesdoo_shift_swap/models/res_partner.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from odoo import _, api, models
 from odoo.exceptions import UserError
 
@@ -55,7 +57,8 @@ class ResPartner(models.Model):
         Same utility as get_next_shifts() but return beesdoo.shift.template.dated
         :return: beesdoo.shift.template.dated recordset
         """
-        shifts = self.get_next_shifts()
+        generated_shifts, planned_shifts = self.get_next_shifts()
+        shifts = generated_shifts + planned_shifts
         return self.env["beesdoo.shift.template.dated"].swap_shift_to_tmpl_dated(shifts)
 
     @api.multi
@@ -101,3 +104,114 @@ class ResPartner(models.Model):
             raise UserError(_("Maximum number of shifts per day reached"))
         if shift_in_month >= max_shift_per_month:
             raise UserError(_("Maximum number of shifts per month reached"))
+
+    def get_next_shifts(self):
+        """
+        Override get_next_shifts method to take into account
+        shift exchanges and solidarity shifts
+        """
+        generated_shifts, planned_shifts = super(ResPartner, self).get_next_shifts()
+
+        # Get all the changes related to self
+        swaps = self.env["beesdoo.shift.swap"].search([("worker_id", "=", self.id)])
+        exchange_requests = self.env["beesdoo.shift.exchange_request"].search(
+            [
+                ("worker_id", "=", self.id),
+                ("status", "=", "done"),
+            ]
+        )
+        solidarity_offers = self.env["beesdoo.shift.solidarity.offer"].search(
+            [
+                ("worker_id", "=", self.id),
+                ("state", "=", "validated"),
+            ]
+        )
+        solidarity_requests = self.env["beesdoo.shift.solidarity.request"].search(
+            [
+                ("worker_id", "=", self.id),
+                ("state", "=", "validated"),
+            ]
+        )
+
+        # Swaps
+        for swap in swaps:
+            planned_shifts = self.exchange_shifts(
+                generated_shifts,
+                planned_shifts,
+                swap.exchanged_tmpl_dated_id,
+                swap.wanted_tmpl_dated_id,
+            )
+
+        # Exchanges
+        for request in exchange_requests:
+            planned_shifts = self.exchange_shifts(
+                generated_shifts,
+                planned_shifts,
+                request.exchanged_tmpl_dated_id,
+                request.validate_request_id.exchanged_tmpl_dated_id,
+            )
+
+        # Solidarity offers
+        for offer in solidarity_offers:
+            planned_shifts = self.exchange_shifts(
+                generated_shifts, planned_shifts, wanted_tmpl_dated=offer.tmpl_dated_id
+            )
+
+        # Solidarity requests
+        for sol_request in solidarity_requests:
+            planned_shifts = self.exchange_shifts(
+                generated_shifts,
+                planned_shifts,
+                exchanged_tmpl_dated=sol_request.tmpl_dated_id,
+            )
+
+        return generated_shifts, planned_shifts
+
+    def exchange_shifts(
+        self,
+        generated_shifts,
+        planned_shifts,
+        exchanged_tmpl_dated=None,
+        wanted_tmpl_dated=None,
+    ):
+        """
+        Update the personal future shifts list by exchanging two shifts.
+        Parameters generated_shifts and planned_shifts are generated
+        by method get_next_shifts.
+        exchanged_tmpl_dated is the dated template the user has to be unsubscribed from.
+        wanted_tmpl_dated is the dated template the user has to be subscribed to.
+        One of these two parameters can be None if there are no shifts
+        to subscribe/unsubscribe.
+
+        :param generated_shifts: beesdoo.shift.shift list
+        :param planned_shift: beesdoo.shift.shift list
+        :param exchanged_tmpl_dated: beesdoo.shift.template.dated
+        :param wanted_tmpl_dated: beesdoo.shift.template.dated
+        :return planned_shift: beesdoo.shift.shift list
+        """
+        self.ensure_one()
+        if exchanged_tmpl_dated and exchanged_tmpl_dated.date > datetime.now():
+            # Remove the exchanged shift from the list if it is not yet generated
+            for shift in planned_shifts:
+                if (
+                    shift.task_template_id == exchanged_tmpl_dated.template_id
+                    and shift.start_time == exchanged_tmpl_dated.date
+                ):
+                    planned_shifts.remove(shift)
+
+        if wanted_tmpl_dated and wanted_tmpl_dated.date > datetime.now():
+            # Search if wanted shift is generated
+            new_shift_generated = False
+            for shift in generated_shifts:
+                if (
+                    shift.task_template_id == wanted_tmpl_dated.template_id
+                    and shift.start_time == wanted_tmpl_dated.date
+                ):
+                    new_shift_generated = True
+                    break
+            # If not, create it and add it to the list
+            if not new_shift_generated:
+                new_shift = wanted_tmpl_dated.new_shift(self)
+                planned_shifts.append(new_shift)
+
+        return planned_shifts
