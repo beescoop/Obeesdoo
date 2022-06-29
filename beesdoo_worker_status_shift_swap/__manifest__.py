60ca9ade7b9a91ca05c7e8912cc7b49f1ab971fd
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 14:21:30 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- beesdoo_worker_status_shift_swap/__manifest__.py
+++ beesdoo_worker_status_shift_swap/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020 Coop IT Easy SCRLfs
+# Copyright 2020 Coop IT Easy SC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Beescoop Worker Status manager for beesdoo_shift_swap",
     "summary": """Worker status management specific to shift exchanges.""",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Cooperative management",
     "version": "12.0.1.0.0",
