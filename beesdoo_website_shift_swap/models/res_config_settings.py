85046178b6a9ae30465dd042fdd45ba60a70932e
MatteoLdx <com.mldx@ik.me>
2022-07-01 11:24:21 +0200

    [IMP] b_website_shift: don't display 'show next shifts' button if there are no next shifts

--- beesdoo_website_shift_swap/models/res_config_settings.py
+++ beesdoo_website_shift_swap/models/res_config_settings.py
@@ -5,7 +5,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     request_number_limit = fields.Integer(
-        string="Maximum number of requests (per category)"
+        string="Maximum number of requests (per category) "
         "displayed on page 'My requests'",
         config_parameter="beesdoo_website_shift_swap.request_number_limit",
     )
