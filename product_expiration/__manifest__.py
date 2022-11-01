01172a13356013ed3bc7843969fd04395caa0ed8
Victor Champonnois <victor@coopiteasy.be>
2022-11-01 12:19:34 +0100

    [REF] split beesdoo_product
    into:
    - product_ingredients.
    - product_hazard.
    - sale_product_deposit.
    - product_expiration.
    - product_sale_limit_date.
    - product_scale_label.
    - product_barcode_generator.
    - beesdoo_product_label
    - product_label_print_request

---
+++ product_expiration/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+#   - Elouan Lebars <elouan@coopiteasy.be>
+#   - Rémy Taymans <remy@coopiteasy.be>
+#   - Houssine BAKKALI <houssine@coopiteasy.be>
+#   - Manuel Claeys Bouuaert <manuel@coopiteasy.be>
+#   - Vincent Van Rossem <vincent@coopiteasy.be>
+#   - Elise Dupont
+#   - Thibault François
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Expiration",
+    "summary": """Add Number of Days Before Product Expiration.""",
+    "author": "BEES coop - Cellule IT, Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Sales",
+    "version": "12.0.2.0.0",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
