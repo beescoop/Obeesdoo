852a1f5cb2ffe691319129332457cff898685d93
Victor Champonnois <victor@coopiteasy.be>
2022-11-07 15:43:31 +0100

    [REF] split beesdoo_product_usability->product_main_supplier

--- beesdoo_product_usability/__manifest__.py
+++ beesdoo_product_usability/__manifest__.py
@@ -2,18 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "BEES coop Product Usability",
-    "summary": """
-        Adapt the product views.
-    """,
+    "summary": """Simplification of Product View.""",
     "author": "BEES coop - Cellule IT, Coop IT Easy SC",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Sales Management",
     "version": "12.0.1.1.0",
     "depends": [
-        "beesdoo_product",
-        "beesdoo_stock_coverage",
+        "product_main_supplier",
         "beesdoo_purchase",
     ],
-    "data": ["views/beesdoo_product.xml"],
+    "data": [
+        "views/beesdoo_product.xml",
+    ],
     "license": "AGPL-3",
 }
