7b1d471568194b85a94afe9d10d80876f8cf10ff
Robin Keunen <robin@keunen.net>
2022-11-15 11:58:49 +0100

    [REF] rename beesdoo_pos_coop_status -> pos_coop_status

---
+++ pos_shift_partner_can_shop/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "POS - Can Partner Shop",
+    "summary": """Display in the POS whether the partner can shop or not.""",
+    "author": "Coop IT Easy SC",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "category": "Point Of Sale",
+    "version": "12.0.1.0.0",
+    "depends": ["point_of_sale", "beesdoo_shift"],
+    "qweb": ["static/src/xml/pos.xml"],
+    "data": ["views/assets.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ pos_shift_partner_can_shop/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module displays in the pos whether the partner can shop or not.
+If the partner  cannot shop or is not set, a confirm popup
+is shown.
