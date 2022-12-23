0f86c644d2abf0723de41570ac8b123a6318a820
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2022-12-23 13:52:50 +0100

    [ADD] beesdoo_website_posorder_amount: Emptied module
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ beesdoo_website_posorder_amount/__manifest__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Portal Website POS order amount",
+    "summary": """
+        Emptied.""",
+    "version": "12.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "portal_posorder_amount",
+    ],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
