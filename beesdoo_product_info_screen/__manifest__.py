ff006a131bdce39a323fbc5156d1d8e6334f7b5c
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2022-12-23 11:02:25 +0100

    [ADD] beesdoo_product_info_screen: Emptied module
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ beesdoo_product_info_screen/__manifest__.py
@@ -0,0 +1,23 @@
+# SPDX-FileCopyrightText: 2022 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Product Information Screen",
+    "summary": """
+        Emptied.""",
+    "version": "12.0.0.0.1",
+    "category": "Product",
+    "website": "https://github.com/beescoop/Obeesdoo",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "product_info_screen",
+    ],
+    "excludes": [],
+    "data": [],
+    "demo": [],
+    "qweb": [],
+}
