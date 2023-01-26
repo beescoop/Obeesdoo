18f7f5dd72147f5ec6a6944abd22d11e2c146d6d
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-01-26 11:12:36 +0100

    [FIX] beesdoo_purchase: Repair dependencies
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- beesdoo_purchase/__manifest__.py
+++ beesdoo_purchase/__manifest__.py
@@ -9,10 +9,8 @@
     "category": "Purchase",
     "version": "12.0.1.4.0",
     "depends": [
-        "base",
         "purchase",
-        "beesdoo_product",
-        "beesdoo_stock_coverage",
+        # beesdoo_purchase was (almost entirely) split into the subsequent modules.
         "purchase_order_responsible",
         "account_invoice_line_custom_reference",
         "purchase_order_main_supplier",
