da7d18603492908a14345ae6ea08d2b0d93e256e
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-03-09 12:27:03 +0100

    [FIX] shift: "instanciate" typo
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- shift/wizard/__init__.py
+++ shift/wizard/__init__.py
@@ -3,5 +3,5 @@ from . import assign_super_coop
 from . import batch_template
 from . import extension
 from . import holiday
-from . import instanciate_planning
+from . import instantiate_planning
 from . import temporary_exemption
