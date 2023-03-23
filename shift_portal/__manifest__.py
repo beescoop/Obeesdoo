5d65f6ee00eee1e86608d7c844f003acb09b48d3
Victor Champonnois <victor@coopiteasy.be>
2023-03-23 15:51:14 +0100

    [MIG] shift_portal: Migration to 13.0

--- shift_portal/__manifest__.py
+++ shift_portal/__manifest__.py
@@ -11,7 +11,7 @@
     """,
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "version": "12.0.2.1.0",
+    "version": "13.0.1.0.0",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Cooperative management",
     "depends": ["portal", "website", "shift"],
