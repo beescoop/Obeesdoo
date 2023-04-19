86b825cb168558ef3974d23a158c6c24eeb79fe0
Victor Champonnois <victor@coopiteasy.be>
2023-04-19 09:57:55 +0200

    [ADD] shift_structure : structure and is_structure

---
+++ shift_structure/__manifest__.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Shift Structure",
+    "summary": """
+        Add a 'structure' field on shifts""",
+    "version": "16.0.1.0.0",
+    "category": "Cooperative management",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SC",
+    "maintainers": ["victor-champonnois"],
+    "license": "AGPL-3",
+    "application": False,
+    "depends": [
+        "shift",
+        "shift_portal",
+    ],
+    "data": [
+        "views/task.xml",
+        "views/task_template.xml",
+        "views/res_partner.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
