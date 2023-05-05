82bda17fd60461a81abb1e16be4092d45571c16b
Victor Champonnois <victor@coopiteasy.be>
2023-05-05 17:21:12 +0200

    fixup! [ADD] unit tests

--- shift_structure/tests/test_shift_structure.py
+++ shift_structure/tests/test_shift_structure.py
@@ -10,7 +10,8 @@ class TestShiftStructure(TestShiftCommon):
         super().setUpClass()
         cls.WebsiteShiftController = WebsiteShiftController()
         cls.website = cls.env['website'].browse(1)
-
+        cls.task_template = cls.env.ref("shift.task_template_1_demo")
+        cls.free_space = 100
 
     def test_get_selected_structure_if_no_structure(self):
         """if requestion.session["structure"] is empty,
@@ -29,35 +30,45 @@ class TestShiftStructure(TestShiftCommon):
             selected_structure = self.WebsiteShiftController.get_selected_structure()
             self.assertEqual(selected_structure, self.env["res.partner"])
 
-    def test_filter_shift_by_structure_if_structure(self):
+    def test_compute_display_shift_if_template_has_other_structure(self):
         """only the shifts with structure equals to the selected_structure
         in request.session must be returned by
         filter_available_shift_irregular_worker"""
-        self._generate_shifts()
         with MockRequest(self.env, website=self.website) as request:
-            shifts = self.WebsiteShiftController.get_future_shifts_with_no_worker()
             request.session["structure"] = "1"
-            # shifts[0].structure = 
-            subscribed_shifts = self.WebsiteShiftController.my_subscribed_shifts()
-            highlight_rule_pc = request.website.highlight_rule_pc
-            hide_rule = request.website.hide_rule / 100.0
+            request.website.hide_rule = 0
+            self.assertFalse(
+                self.WebsiteShiftController.compute_display_shift(
+                    self.free_space,
+                    self.task_template
+                    )
+                )
+
+    def test_compute_display_shift_template_has_correct_structure(self):
+        """only the shifts with structure equals to the selected_structure
+        in request.session must be returned by
+        filter_available_shift_irregular_worker"""
+        self.task_template.structure = self.env['res.partner'].browse(1)
+        with MockRequest(self.env, website=self.website) as request:
+            request.session["structure"] = "1"
+            request.website.hide_rule = 0
+            self.assertTrue(
+                self.WebsiteShiftController.compute_display_shift(
+                    self.free_space,
+                    self.task_template
+                    )
+                )
 
 
-
-    def test_filter_shift_by_structure_if_no_structure(self,):
+    def test_compute_display_shift_if_no_structure(self,):
         """if requestion.session["structure"] is empty
-        filter_available_shift_irregular_worker should returns all shifts"""
-        self._generate_shifts()
+        compute_display_shift should always return True"""
         with MockRequest(self.env, website=self.website) as request:
             request.session["structure"] = ""
-            shifts = self.WebsiteShiftController.get_future_shifts_with_no_worker()
-            subscribed_shifts = self.WebsiteShiftController.my_subscribed_shifts()
-            highlight_rule_pc = request.website.highlight_rule_pc
-            hide_rule = 0 # display all shifts
-            displayed_shifts = self.WebsiteShiftController.filter_available_shift_irregular_worker(
-                shifts,
-                subscribed_shifts,
-                highlight_rule_pc,
-                hide_rule
-            )
-            self.assertEqual(len(displayed_shifts),len(shifts))
+            request.website.hide_rule = 0
+            self.assertTrue(
+                self.WebsiteShiftController.compute_display_shift(
+                    self.free_space,
+                    self.task_template
+                    )
+                )
