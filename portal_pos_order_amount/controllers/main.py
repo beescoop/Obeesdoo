3997a38c602a61304308885291114c26a65676f9
Victor Champonnois <victor@coopiteasy.be>
2023-05-12 14:01:07 +0200

    [REF] rename beesdoo_website_posorder_amount to portal_posorder_amount
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ portal_pos_order_amount/controllers/main.py
@@ -0,0 +1,49 @@
+# Copyright 2018 Rémy Taymans <remytaymans@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from itertools import groupby
+
+from odoo.http import request
+from odoo.tools import float_repr
+
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class PortalPosOrderAmount(CustomerPortal):
+    def _prepare_portal_layout_values(self):
+        values = super(PortalPosOrderAmount, self)._prepare_portal_layout_values()
+        user = request.env.user
+        owned_pos_order = (
+            request.env["pos.order"]
+            .sudo()
+            .search(
+                [
+                    (
+                        "partner_id",
+                        "=",
+                        user.partner_id.commercial_partner_id.id,
+                    ),
+                    ("state", "!=", "cancel"),
+                ]
+            )
+        )
+        values["pos_order_amount"] = float_repr(
+            sum(po.amount_total for po in owned_pos_order), 2
+        )
+        values["pos_order_amount_by_year"] = [
+            {
+                "year": year,
+                "amount": float_repr(
+                    sum(pos_order.amount_total for pos_order in grouped_pos_orders),
+                    2,
+                ),
+            }
+            for year, grouped_pos_orders in groupby(
+                owned_pos_order, key=lambda pos_order: pos_order.date_order.year
+            )
+        ]
+        values["company_currency"] = (
+            request.env["res.company"]._company_default_get().currency_id
+        )
+        return values
