22d39a5fd9b2871215fd9d9cfeda1d967938c12e
Victor Champonnois <victor@coopiteasy.be>
2023-05-12 14:01:07 +0200

    [REF] rename beesdoo_website_posorder_amount to portal_posorder_amount

--- portal_pos_order_amount/controllers/main.py
+++ portal_pos_order_amount/controllers/main.py
@@ -14,7 +14,7 @@ class PortalPosOrderAmount(CustomerPortal):
     def _prepare_portal_layout_values(self):
         values = super(PortalPosOrderAmount, self)._prepare_portal_layout_values()
         user = request.env.user
-        owned_posorder = (
+        owned_pos_order = (
             request.env["pos.order"]
             .sudo()
             .search(
@@ -28,10 +28,10 @@ class PortalPosOrderAmount(CustomerPortal):
                 ]
             )
         )
-        values["posorder_amount"] = float_repr(
-            sum(po.amount_total for po in owned_posorder), 2
+        values["pos_order_amount"] = float_repr(
+            sum(po.amount_total for po in owned_pos_order), 2
         )
-        values["posorder_amount_by_year"] = [
+        values["pos_order_amount_by_year"] = [
             {
                 "year": year,
                 "amount": float_repr(
@@ -40,7 +40,7 @@ class PortalPosOrderAmount(CustomerPortal):
                 ),
             }
             for year, grouped_pos_orders in groupby(
-                owned_posorder, key=lambda pos_order: pos_order.date_order.year
+                owned_pos_order, key=lambda pos_order: pos_order.date_order.year
             )
         ]
         values["company_currency"] = (
