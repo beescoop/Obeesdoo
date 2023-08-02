cfe226f1d4470bb2158771cb35498c4ebe9057a2
Victor Champonnois <victor@coopiteasy.be>
2023-08-02 14:49:21 +0200

    [MIG] beesdoo_product_label: Migration to 14.0

--- beesdoo_product_label/__manifest__.py
+++ beesdoo_product_label/__manifest__.py
@@ -15,7 +15,7 @@
     "author": "BEES coop - Cellule IT, Coop IT Easy SC",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Sales",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": [
         "product",
         "account",
