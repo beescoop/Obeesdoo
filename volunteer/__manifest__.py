dc9dc0386f517dd389e8b6a497456faf9d72c3e2
Ayden Mortier <pro@arpm.dev>
2025-03-26 11:17:15 +0100

    Customisation shift views and add chatter

--- volunteer/__manifest__.py
+++ volunteer/__manifest__.py
@@ -20,6 +20,7 @@
         "security/ir.model.access.csv",
         "views/shift_view.xml",
         "views/volunteer_view.xml",
+        "views/shift_participation_form_view.xml",
         "views/shift_category_view.xml",
         "views/shift_type_view.xml",
         "views/shift_tag_view.xml",
