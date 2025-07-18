1215ab499ebff7438a610de3e6f8a1506afc8c81
Simon Hick <sim@coopiteasy.be>
2025-07-18 19:07:14 +0200

    [MIG] beesdoo_product_usability: Migration to 16.0

--- beesdoo_product_usability/__manifest__.py
+++ beesdoo_product_usability/__manifest__.py
@@ -2,15 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "BEES coop Product Usability",
-    "summary": """Emptied.\nLeftover: simplification of Product View.""",
+    "summary": """Simplification of product views""",
     "author": "BEES coop - Cellule IT, Coop IT Easy SC",
     "website": "https://github.com/beescoop/Obeesdoo",
     "category": "Sales Management",
-    "version": "12.0.2.0.1",
-    "depends": [
-        "product_main_supplier",
-        "beesdoo_purchase",
-    ],
+    "version": "16.0.1.0.0",
+    "depends": ["product_main_supplier", "purchase_stock_coverage"],
     "data": [
         "views/beesdoo_product.xml",
     ],
