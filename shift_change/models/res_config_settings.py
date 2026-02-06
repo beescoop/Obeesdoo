c0248f9156b706312d618e99fb26e07157a33d70
Rémy Taymans <remytms@tsmail.eu>
2026-02-06 18:23:44 +0100

    [ADD] shift_change_portal

--- shift_change/models/res_config_settings.py
+++ shift_change/models/res_config_settings.py
@@ -1,16 +1,18 @@
+# SPDX-FileCopyrightText: 2026 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    # enable_exchanges
-    enable_shift_changes = fields.Boolean(
-        string="Activate shift changes",
-        config_parameter="shift_change.enable_shift_changes",
-    )
-    # day_limit_swap
     hour_limit_change = fields.Integer(
         string="Number of hours above which a cooperator cannot change his shift",
         config_parameter="shift_change.hour_limit_change",
+    )
+    same_shift_change_max = fields.Integer(
+        string="Number of time the same shift can be changed",
+        config_parameter="shift_change.same_shift_change_max",
     )
--- shift_change_portal/__init__.py
+++ shift_change_portal/__init__.py
@@ -2,4 +2,5 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from . import test_shift_change
+from . import controllers
+from . import models
