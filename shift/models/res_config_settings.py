f3a619aea88d07c47ba2ae3fe3411b38e84478cb
Rémy Taymans <remytms@tsmail.eu>
2026-03-25 11:30:00 +0100

    [FIX] shift: missing parameter

--- shift/models/res_config_settings.py
+++ shift/models/res_config_settings.py
@@ -7,3 +7,16 @@ class ResConfigSettings(models.TransientModel):
     shift_period = fields.Integer(
         string="Shift period", config_parameter="shift.shift_period"
     )
+
+    min_hours_to_unsubscribe = fields.Integer(
+        string="Minimum number of hours before a shift to unsubscribe",
+        config_parameter="shift.min_hours_to_unsubscribe",
+    )
+    max_shift_per_day = fields.Integer(
+        string="Maximum number of shifts per day for one cooperator",
+        config_parameter="shift.max_shift_per_day",
+    )
+    max_shift_per_month = fields.Integer(
+        string="Maximum number of shifts per month for one cooperator",
+        config_parameter="shift.max_shift_per_month",
+    )
